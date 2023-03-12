c4ccc90c60cea6213b716ac7ceb5abd63452d03f
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2023-03-12 19:45:52 +0100

    Add: Import all global dependencies of already built to local

--- tools/list_deps.sh
+++ tools/list_deps.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+make all
 CURRENT=$(make current)
 CURRENT_E=$(make current_e)
 BIN=$(make current_bin)
 
-find "$CURRENT_E" -iname "*.c" -exec cat {} \; | grep "\.h" | grep -oP '(?<=").*(?=")' | awk '!seen[$0]++'
-readelf -d "$CURRENT/$BIN" | grep 'NEEDED' | grep -oP '(?<=\[).*(?=\])' | xargs whereis | awk '{print $2}'
+I=$(find "$CURRENT_E" -iname "*.c" -exec cat {} \; | grep "\.h" | grep -oP '(?<=").*(?=")' | awk '!seen[$0]++')
+L=$(readelf -d "$CURRENT/$BIN" | grep 'NEEDED' | grep -oP '(?<=\[).*(?=\])' | xargs whereis | awk '{print $2}')
+
+echo "$I" | grep -v "./inc/" | xargs -n 1 dirname | xargs -I {} mkdir -p ./include{}
+echo "$L" | grep -v "./inc/" | xargs -n 1 dirname | xargs -I {} mkdir -p ./include{}
+
+echo "$I" | grep -v "./inc/" | xargs -I {} cp {} ./include{}
+echo "$L" | grep -v "./inc/" | xargs -I {} cp {} ./include{}
+
